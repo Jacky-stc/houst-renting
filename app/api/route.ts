24ce7024eceee152e57d31dfc3236f81d71d29bc
Jacky Su <jacky880311@gmail.com>
2024-10-12 03:31:14 +0800

    feat:add update google sheet data function and api endpoint

--- app/api/route.ts
+++
@@ -1,7 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function POST(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
-  console.log(req.method);
-  return res.status(200).send({ message: "ok" });
-}

a74fec3c0d3825ea543116564c0013e545734f19
Jacky Su <jacky880311@gmail.com>
2025-06-17 00:39:08 +0800

    fix: fix import path problem.

--- app/components/house/MobileHouseInfo.tsx
+++ app/components/house/MobileHouseInfo.tsx
@@ -1,9 +1,9 @@
 "use client";
 import React, { useEffect, useRef } from "react";
-import useDrag from "../lib/useDrag";
-import HouseInfo from "./house/HouseInfo";
-import { useRentingData } from "../store/useRentingData";
 import { useShallow } from "zustand/shallow";
+import useDrag from "@/app/lib/useDrag";
+import { useRentingData } from "@/app/store/useRentingData";
+import HouseInfo from "./HouseInfo";
 
 const MobileHouseInfo = () => {
   const targetRef = useRef<HTMLDivElement>(null);

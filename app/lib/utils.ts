89f45b8f40f6c54d7a67bffdfa176fc956b4ac71
Jacky Su <jacky880311@gmail.com>
2026-06-05 04:12:38 +0800

    feat: update new employee's name into personList for data fetching.

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -87,24 +87,25 @@ export const regionList: { [key: string]: string } = {
 };
 
 export const personList: { name: PersonName; BGColor: string }[] = [
-  {
-    name: '承恩',
-    BGColor: 'linear-gradient(90deg, rgb(69 130 167) 0%, rgb(87 93 159) 50%, rgb(56 56 168) 100%)',
-  },
-  { name: '黑', BGColor: 'linear-gradient(to right, #485563, #29323c)' },
-  {
-    name: 'K',
-    BGColor: 'linear-gradient(90deg, rgb(175 19 19) 0%, rgb(186 47 47) 50%, rgb(255 113 47) 100%)',
-  },
-  {
-    name: '查',
-    BGColor: 'linear-gradient(90deg, rgb(101 69 34) 0%, rgb(168 141 106) 50%, rgb(171 156 149) 100%)',
-  },
+  // {
+  //   name: '承恩',
+  //   BGColor: 'linear-gradient(90deg, rgb(69 130 167) 0%, rgb(87 93 159) 50%, rgb(56 56 168) 100%)',
+  // },
+  // { name: '黑', BGColor: 'linear-gradient(to right, #485563, #29323c)' },
+  // {
+  //   name: 'K',
+  //   BGColor: 'linear-gradient(90deg, rgb(175 19 19) 0%, rgb(186 47 47) 50%, rgb(255 113 47) 100%)',
+  // },
+  // {
+  //   name: '查',
+  //   BGColor: 'linear-gradient(90deg, rgb(101 69 34) 0%, rgb(168 141 106) 50%, rgb(171 156 149) 100%)',
+  // },
+  // { name: '小豪', BGColor: 'linear-gradient(to right, #1a2980, #26d0ce)' },
   { name: '阿聖', BGColor: 'linear-gradient(to right, #780206, #061161)' },
   {
     name: '至倫',
     BGColor: 'linear-gradient(to right, #aa4b6b, #6b6b83, #3b8d99)',
   },
-  { name: '小豪', BGColor: 'linear-gradient(to right, #1a2980, #26d0ce)' },
   { name: '皇佑', BGColor: 'linear-gradient(to right, #1a2980, #26d0ce)' },
+  { name: '阿凱', BGColor: 'linear-gradient(to right, #485563, #29323c)' },
 ];
